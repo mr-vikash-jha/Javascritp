1ad7ff606c41c83ad63b3823121836e4bcf36ad1
vikash jha <vikashjha@vikashs-MacBook-Pro.local>
2023-12-27 12:16:42 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+// console.log("vikash jha")
+
+
+// number => 2 to power 53
+
+// bigint
+
+// string => ""
+
+// boolean => true
+
+// null => standalone value
+
+// undefined => 
+
+// symbol =>unique
+
+// object =>
+
+
+console.log(typeof "vikash")
+
+console.log(typeof null) // object
+
+console.log(typeof undefined)  // undefined
