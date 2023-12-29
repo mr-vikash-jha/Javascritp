eb06e23993280727a958653dc4d72a4da8c497a3
vikash jha <vikashjha@vikashs-MacBook-Pro.local>
2023-12-29 11:41:10 +0530

    comparisions types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let value = 3
+let negValue = -value
+
+// console.log(negValue)
+
+
+
+let str1 = "Hello"
+
+let str2 = " vikash"
+
+let str3 = str1 + str2
+
+// console.log(str3)
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+
+// console.log("1" + 2 + 2);
+
+
+// console.log(1 + 2 + "3")
+
+console.log(true);
+
+console.log(+true);
+
